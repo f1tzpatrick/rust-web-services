14275e3f6a2e416b1223aef24e9ba1d12bb489e7
Steven Fitzpatrick <steven@fitz.gg>
2022-01-23 05:17:50 -0600

    Add connection to local MySQL

--- backend/webservice/src/main.rs
+++ backend/webservice/src/main.rs
@@ -4,6 +4,8 @@ mod product;
 
 use rocket::fairing::AdHoc;
 use rocket::serde::Deserialize;
+use rocket_sync_db_pools::{diesel, database};
+
 
 type ProductFilePath = String;
 
@@ -13,10 +15,14 @@ struct AppConfig {
     products_file: ProductFilePath,
 }
 
+#[database("productdb")]
+struct ProductDbConn(diesel::MysqlConnection);
+
 #[launch]
 fn rocket() -> _ {
     println!("Hello, world!");
     rocket::build()
         .attach(product::service::stage())
         .attach(AdHoc::config::<AppConfig>())
+        .attach(ProductDbConn::fairing())
 }
